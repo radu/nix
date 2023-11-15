917f6f6945843a602a07d69907d48ca790aa571f
radu <radu@raduta.net>
2023-11-15 21:32:52 +0000

    wsl flake

--- flake.nix
+++ flake.nix
@@ -9,9 +9,13 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    NixOS-WSL = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, NixOS-WSL, ... }@inputs: 
   {
     nixosConfigurations = {
       "lap" = nixpkgs.lib.nixosSystem {
@@ -24,11 +28,26 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.radu = import ./home/home.nix;
           }
         ];
       };
+      radu-p14s = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { nix.registry.nixpkgs.flake = nixpkgs; }
+          ./configuration-wsl.nix
+          NixOS-WSL.nixosModules.wsl
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.radu = import ./home/home.nix;
+          }
+        ];
+      }; 
     };
   };
 }
